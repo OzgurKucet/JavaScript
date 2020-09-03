f244a6fcc7f535cb754e3ffc0b1000a6150dc473
Ozgur Kucet <ozgurkucet@hotmail.com>
2020-09-03 13:37:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//npm run start diyerek çalıştır packege json da yazdım bu kodu...
+console.log("Merhaba ");
+
+//GitHuba atarken modülleri atmak sıkıntıysa silebiliriz çünkü artık package.jsonda var zaten
+
+//Eger modulleri silsek bile package.jsonda old için npm install dememiz yeterli...
+
+//Sadece development depandence olarak eklemek istersek -
+//Yani Sadece geliştirme ortamında kullanmak istersek
+//npm install webpack-cli --save-dev diyebiliriz...
